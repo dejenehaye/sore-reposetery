dd71583d8dc2f08fb9961ee0414027e5ffd368c0
dejenehaye <kiae2005@yahoo.com>
2020-08-24 10:39:45 -0400

    commit

---
+++ MyfirstProject/src/pack/com/MyFirstClass.java
@@ -0,0 +1,11 @@
+package pack.com;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+		System.out.println("hello world");
+	}
+
+}
